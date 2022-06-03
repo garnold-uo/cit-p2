cf6de2a6abc3fb320d02e1a64821061b236d16c1
garnold-uo <105458607+garnold-uo@users.noreply.github.com>
2022-06-03 12:37:11 -0700

    Add files via upload

---
+++ p2-random.js
@@ -0,0 +1,46 @@
+/*
+    CIT 281 Project 2
+    Name: Garrett Arnold
+*/
+
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+//get random string between 5 and 25
+function getString() {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+    let result = "";
+    let lengthOfOutputString = getRandomInteger(5, 26);
+
+    for (let i = 0; i < lengthOfOutputString; i++) {
+        result += getRandomLetter();
+    }
+    console.log(result);
+}
+getString()
+
+// Get random letter from alphabet array
+function getRandomLetter() {
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    let emptyArray = [];
+
+    let letter = alphabet[Math.floor(Math.random()*alphabet.length)];
+    return letter
+}
+
+// Get random string between 10 and 20
+function getRandomString(minLength, maxLength) {
+    let result = "";
+    let lengthOfOutputString = getRandomInteger(minLength, maxLength);
+    for (let i = 0; i < lengthOfOutputString; i++) {
+        result += getRandomLetter();
+    }
+    console.log(result);
+}
+
+getRandomString(10, 21)
+
+// get string in ascending order
+const getSortedString = (string) => string.split('').sort().join('');
+console.log(getSortedString("hello world"));
